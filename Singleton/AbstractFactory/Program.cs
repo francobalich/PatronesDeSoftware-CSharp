1a7c318673fab320f77753488bbd8858bb60fa96
Franco Balich <franco.balich@gmail.com>
2023-04-27 00:20:03 -0300

    Add Abstract Factory

---
+++ Singleton/AbstractFactory/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pizzeria fabrica;
+            fabrica = new PizzeriaArgentina();
+
+            Pizza pizza = fabrica.CrearPizza();
+            Empanada empanada = fabrica.CrearEmpanada();
+            Console.WriteLine($"{pizza.ToString()}, {empanada.ToString()}");
+
+            fabrica = new PizzeriaItaliana();
+            pizza = fabrica.CrearPizza();
+            empanada = fabrica.CrearEmpanada();
+            Console.WriteLine($"{pizza.ToString()}, {empanada.ToString()}");
+
+            fabrica = new PizzeriaEspañola();
+            pizza = fabrica.CrearPizza();
+            empanada = fabrica.CrearEmpanada();
+            Console.WriteLine($"{pizza.ToString()}, {empanada.ToString()}");
+
+            Console.ReadKey();
+        }
+    }
+}

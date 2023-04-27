1c9fd3443653fc660746666ba96a2bea2ac6a1db
Franco Balich <franco.balich@gmail.com>
2023-04-26 23:52:35 -0300

    Add Singleton

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario usuario = new Usuario();
+
+            try
+            {
+                Console.WriteLine("Ingrese su nombre:");
+                string nombre = Console.ReadLine();
+                Console.WriteLine("Ingrese su contraseña:");
+                string contraseña = Console.ReadLine();
+
+                usuario.Nombre = nombre;
+                usuario.Contraseña = contraseña;
+
+                SessionManager.Login(usuario);
+                SessionManager u = SessionManager.GetInstance;
+                SessionManager.Logout();
+                
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}

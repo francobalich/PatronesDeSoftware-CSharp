976f1d4a1360e4a25b699d5ef9f9f238162d9b5f
Franco Balich <franco.balich@gmail.com>
2023-05-10 20:25:02 -0300

    Add Memento

---
+++ Patrones/Memento/Memento.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    public class Memento
+    {
+        private string _estado;
+
+        public Memento(string estado)
+        {
+            _estado = estado;
+        }
+
+        public string Estado
+        {
+            get
+            {
+                return _estado;
+            }
+        }
+    }
+}
---
+++ Patrones/Memento/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    internal class Program
+    {
+        static CareTaker caretaker = new CareTaker();
+        static void Main(string[] args)
+        {
+            Persona persona = new Persona();
+            persona.Nombre = "Franco";
+            caretaker.Add(persona.saveToMemento());
+
+            persona.Nombre = "Juan";
+            caretaker.Add(persona.saveToMemento());
+
+            persona.Nombre = "Pedro";
+            caretaker.Add(persona.saveToMemento());
+
+            Memento m1 = caretaker.GetMemento(0);
+            Console.WriteLine("Viendo memento 1: " + m1.Estado);
+            Console.WriteLine("Viendo memento 2: " + caretaker.GetMemento(1).Estado);
+            Console.WriteLine("Viendo memento 3: " + caretaker.GetMemento(2).Estado);
+
+            persona.restoreToMemento(m1);
+            persona.restoreToMemento(caretaker.GetMemento(1));
+            Console.ReadKey();
+        }
+    }
+}
